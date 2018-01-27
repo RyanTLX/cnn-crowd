612dc00a0e620513fa127604f7cf4c23ff4f4abc
Tan Ryan <Ryan@Ryans-MacBook-Pro.local>
2018-01-27 23:56:27 +0800

    Add --cam argument to CreateDataset.py and tqdm

--- CreateDataset.py
+++ CreateDataset.py
@@ -5,11 +5,14 @@ import argparse
 from random import shuffle
 from tqdm import tqdm
 
+tf.logging.set_verbosity(tf.logging.ERROR)
 
-TRAIN_DIR = 'Dataset/train/camall'
-VAL_DIR = 'Dataset/validate/camall'
-TEST_DIR = 'Dataset/test/camall'
+
 IMAGE_HEIGHT = int()
+CAM = str()
+TRAIN_DIR = str()
+VAL_DIR = 'Dataset/validate/' + CAM
+TEST_DIR = 'Dataset/test/' + CAM
 
 
 def create_label(image_name):
@@ -44,15 +47,15 @@ def create_dataset(to_height, set_type):
         if not files:
             continue
 
-        for img in tqdm(files):
+        for img in tqdm(files, total=len(files), unit='files'):
             path = os.path.join(dirpath, img)
             img_data = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
             new_width = to_height * int(img_data.shape[1] / img_data.shape[0])
             img_data = cv2.resize(img_data, (to_height, new_width))
-            data.append([np.array(img_data), create_label(img)])
+            data.append([np.array(img_data), create_label(img), img])
 
     shuffle(data)
-    file_name = str(to_height) + '_' + set_type + '_all_data.npy'
+    file_name = str(to_height) + '_' + CAM + '_' + set_type + '_dataset.npy'
     save_path = os.path.join('Dataset', file_name)
     np.save(save_path, data)
 
@@ -62,16 +65,23 @@ def create_dataset(to_height, set_type):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--image_height", help="height of image", type=int, required=True)
+    parser.add_argument("--cam", help="which camera set to train", type=str, required=True)
     args = parser.parse_args()
 
     if args.image_height:
         IMAGE_HEIGHT = args.image_height
+    if args.cam:
+        CAM = args.cam
 
-    print('Creating train dataset: ')
+    TRAIN_DIR = 'Dataset/train/' + CAM
+    VAL_DIR = 'Dataset/validate/' + CAM
+    TEST_DIR = 'Dataset/test/' + CAM
+
+    print('Creating train dataset from: ' + TRAIN_DIR)
     print('Train data set saved at: ' + create_dataset(IMAGE_HEIGHT, 'train'))
 
-    print('Creating validate dataset: ')
+    print('Creating validate dataset from: ' + VAL_DIR)
     print('validate data set saved at: ' + create_dataset(IMAGE_HEIGHT, 'validate'))
 
-    print('Creating test dataset: ')
+    print('Creating test dataset from: ' + TEST_DIR)
     print('Test data set saved at: ' + create_dataset(IMAGE_HEIGHT, 'test'))
