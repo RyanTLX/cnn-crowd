0501b88e991003e6fc1b0babc004afacb7405513
Your Name <you@example.com>
2018-02-05 18:08:25 +0800

    Add static display of images in web app
    Selects 6 random images from dataset folder to display in web app.

--- CrowdEstimatorWeb/CrowdEstimatorWeb/views.py
+++ CrowdEstimatorWeb/CrowdEstimatorWeb/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+import os
+import random
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    params = []
+
+    # Get 6 random images from CrowdDataset folder.
+    dataset_path = 'CrowdEstimatorWeb/static/CrowdEstimatorWeb/CrowdImages'
+    files = [f for f in os.listdir(dataset_path) if not f.startswith('.')]
+
+    for i in range(6):
+        image_name = files[random.randint(1, len(files))]
+        label = image_name.split('_')[0] # CHANGE TO PREDICTION
+        color = "success"
+        if label == "medium":
+            color = "warning"
+        elif label == "high":
+            color = "danger"
+        params.append({'image_name': image_name, 'label': label, 'color': color})
+
+    return render(request, 'CrowdEstimatorWeb/index.html', {'params': params})
